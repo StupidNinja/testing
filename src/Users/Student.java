db409d5b4a478b05306a0bbc2e61c42cf2e60faa
StupidNinja <temirkhan.adilet05@gmail.com>
2024-12-06 13:42:57 +0500

    feat: add getter/setter for Students class

--- src/Users/Student.java
+++ src/Users/Student.java
@@ -1,5 +1,20 @@
 package Users;
 
 public class Student {
+	private int yearsOfStudy;
+
+	/**
+	 * @return the yearsOfStudy
+	 */
+	public int getYearsOfStudy() {
+		return yearsOfStudy;
+	}
+
+	/**
+	 * @param yearsOfStudy the yearsOfStudy to set
+	 */
+	public void setYearsOfStudy(int yearsOfStudy) {
+		this.yearsOfStudy = yearsOfStudy;
+	}
 
 }
